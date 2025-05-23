512f09da2ca65bbaa77dbc2ec35392c10f172a5d
muhammadrandyano <muhammadrandyyano@gmail.com>
2025-05-23 08:10:23 +0700

    Membuat class Stack private and public

--- file1.cpp
+++ file1.cpp
@@ -12,3 +12,25 @@ class Node
         next = NULL;
     }
 };
+
+class Stack
+{
+    private :
+    Node *top;
+
+    public :
+    Stack()
+    {
+        top = NULL;
+    }
+
+    int push(int value)
+    {
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout << "Push value: " << value << endl;
+        return value;
+    }
+};
