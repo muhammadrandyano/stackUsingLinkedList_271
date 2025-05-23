adff92b55e7d980b6f6e82a6c758f8b1448df1d3
muhammadrandyano <muhammadrandyyano@gmail.com>
2025-05-23 08:39:36 +0700

    Membuat void peek

--- file1.cpp
+++ file1.cpp
@@ -46,4 +46,23 @@ class Stack
         cout << "Popped value: " << top->data << endl;
         delete temp;
     }
+
+
+    void peek()
+    {
+        if (top = NULL)
+        {
+            cout << "List is empaty, " << endl;
+        }
+        else
+        {
+            Node*current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        }
+    }
 };
